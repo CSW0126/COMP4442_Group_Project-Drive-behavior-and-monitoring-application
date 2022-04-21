26910ed8e3e53b88548ed4a70d792964f8213e36
CSW0126 <shuiwing123@gmail.com>
2022-04-21 19:20:42 +0800

    chart

--- FlaskApp/website/views.py
+++ FlaskApp/website/views.py
@@ -19,6 +19,7 @@ def home():
 
 @views.route('/Summary', methods=['GET', 'POST'])
 def summary():
+    list = ['Driver ID', 'Car Plate Number', 'Abrupt acceleration times', 'Abrupt Brake Times', 'Neutral Sliding Times', 'Total Neutral Sliding Times (s)', 'Overspeed Times', 'Total Overspeed Times(s)', 'Fatigue Driving TImes', 'Hthrottle Stop Times', 'Oil Leak Times']
     if request.method == 'POST':
         cursor = database.cursor()
         # SELECT DriverID,CarPlateNumber,DATE(recordDAY),sum(isRapidlySpeedup),sum(isRapidlySlowdown),sum(isNeutralSlide),sum(isNeutralSlideFinished),sum(neutralSlideTime),sum(isOverspeed),sum(isOverspeedFinished),sum(overspeedTime),sum(isFatigueDriving),sum(isHthrottleStop),sum(isOilLeak) from `comp4442-group-project`.DrivingRecords  GROUP BY driverID
@@ -28,10 +29,43 @@ def summary():
         end_datetime = str(datetime.strptime(daterange[1], '%Y-%m-%d %I:%M %p'))
         query = "SELECT DriverID,CarPlateNumber,DATE(recordDAY),sum(isRapidlySpeedup),sum(isRapidlySlowdown),sum(isNeutralSlide),sum(isNeutralSlideFinished),sum(neutralSlideTime),sum(isOverspeed),sum(isOverspeedFinished),sum(overspeedTime),sum(isFatigueDriving),sum(isHthrottleStop),sum(isOilLeak) FROM DrivingRecords WHERE recordDAY between '" + start_datetime + "' AND '" + end_datetime + "' GROUP BY driverID"
         cursor.execute(query)
-        result = cursor.fetchall()
-        return render_template("summary.html", results=result, last_search_value=request.form['daterange'])
+        results = cursor.fetchall()
+
+        driverID = []
+        isRapidlySpeedup=[]
+        isRapidlySlowdown=[]
+        NeutralSlidingTimes =[]
+        TotalNeutralSlidingTimes =[]
+        OverspeedTimes = []
+        TotalOverspeedTimes =[]
+        FatigueDrivingTImes =[]
+        HthrottleStopTimes =[]
+        OilLeakTimes =[]
+
+        for result in results:
+            driverID.append(result[0])
+            isRapidlySpeedup.append(result[3])
+            isRapidlySlowdown.append(result[4])
+            NeutralSlidingTimes.append(result[6])
+            TotalNeutralSlidingTimes.append(result[7])
+            OverspeedTimes.append(result[9])
+            TotalOverspeedTimes.append(result[10])
+            FatigueDrivingTImes.append(result[11])
+            HthrottleStopTimes.append(result[12])
+            OilLeakTimes.append(result[13])
+        
+        data = [isRapidlySpeedup,isRapidlySlowdown,NeutralSlidingTimes,TotalNeutralSlidingTimes,OverspeedTimes,TotalOverspeedTimes,FatigueDrivingTImes,HthrottleStopTimes,OilLeakTimes]
+        for i in data:
+            print(i)
+
+        return render_template("summary.html", headerList = list,\
+                                                results=results,\
+                                                driverID=driverID ,\
+                                                chart_data = data,\
+                                                last_search_value=request.form['daterange'])
+         
     else:
-        return render_template("summary.html")
+        return render_template("summary.html", headerList = list)
 
 
 @views.route('/Monitor')
